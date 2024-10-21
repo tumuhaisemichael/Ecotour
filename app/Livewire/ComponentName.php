828002866fef23b4199cbbb2efe7e200dfeae3b6
Aurits <alandaambrose@gmail.com>
2024-10-21 18:53:50 +0300

    added the layouts

--- app/Livewire/ComponentName.php
+++ app/Livewire/ComponentName.php
@@ -2,10 +2,13 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class ComponentName extends Component
 {
+    // layout
+    #[Layout('layouts.tourist')]
     public function render()
     {
         return view('livewire.component-name');
