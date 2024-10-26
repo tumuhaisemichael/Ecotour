c369a33ab66ee74170f91ad2d1cc487296257dec
Aurits <alandaambrose@gmail.com>
2024-10-26 06:32:18 +0300

    tourist layout

--- resources/views/layouts/tourist.blade.php
+++ resources/views/layouts/tourist.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="BESTOURS - Travel and Tours multipurpose template">
+    <meta name="description" content="">
     <meta name="author" content="Ansonika">
     <title>EcoTour</title>
 
@@ -68,15 +68,12 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-md-6 col-sm-6 col-xs-12">
-                            <a href="tel://004542344599" id="phone_top">0045 043204434</a><span id="opening">Mon - Sat
-                                8.00/18.00</span>
+                            <a href="tel://004542344599" id="phone_top">+256 123 456 789</a>
+                            <span id="opening">Mon - Sat 8.00/18.00</span>
                         </div>
                         <div class="col-md-6 col-sm-6 hidden-xs">
                             <ul id="top_links">
-                                <li><a href="#0" id="wishlist_link">Wishlist</a>
-                                </li>
-                                <li><a href="#0">PURCHASE THIS TEMPLATE</a>
-                                </li>
+                                <li><a href="#0" id="wishlist_link">Wishlist</a></li>
                             </ul>
                         </div>
                     </div>
@@ -90,144 +87,39 @@
                 <div class="row">
                     <div class="col-md-3 col-sm-3 col-xs-3">
                         <div id="logo_home">
-                            <h1><a href="index.html" title="City tours travel template">BesTours Travel&amp;Excursion
-                                    Multipurpose Template</a></h1>
+                            <h1><a href="{{ route('tourist.browse-experiences') }}" title="EcoTour Uganda">EcoTour
+                                    Uganda - Community-Driven
+                                    Experiences</a></h1>
                         </div>
                     </div>
-                    <!-- welcome.blade.php -->
-                    <div class="flex items-center justify-center min-h-screen bg-gray-100">
-                        <div class="text-center">
-                            <!-- Button to Login Page or Dashboard -->
-                            <a href="{{ Auth::check() ? route(Auth::user()->role . '.dashboard') : route('login') }}"
-                                class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
-                                {{ Auth::check() ? 'Go to Dashboard' : 'Log In' }}
-                            </a>
-                        </div>
-                    </div>
-
 
                     <nav class="col-md-9 col-sm-9 col-xs-9">
                         <ul id="tools_top">
-                            <li><a href="#" class="search-overlay-menu-btn"><i class="icon-search-6"></i></a>
-                            </li>
+                            <li><a href="#" class="search-overlay-menu-btn"><i class="icon-search-6"></i></a></li>
                         </ul>
-                        <a class="cmn-toggle-switch cmn-toggle-switch__htx open_close"
-                            href="javascript:void(0);"><span>Menu mobile</span></a>
+                        <a class="cmn-toggle-switch cmn-toggle-switch__htx open_close" href="javascript:void(0);">
+                            <span>Menu mobile</span>
+                        </a>
                         <div class="main-menu">
-                            <div id="header_menu">
-                                <img scr="assets/tourist/img/logo_menu.png" width="145" height="34" alt="Bestours">
+                            <div id="header_menu" class="d-flex align-items-center">
+                                <h1 class="m-0 text-primary" style="font-size: 1.5rem;">EcoTour Uganda</h1>
+                                <i class="fas fa-globe ms-2" style="font-size: 1.5rem;"></i>
+                                <!-- Example Font Awesome icon -->
                             </div>
+
                             <a href="#" class="open_close" id="close_in"><i class="icon_set_1_icon-77"></i></a>
                             <ul>
-                                <li class="submenu">
-                                    <a href="javascript:void(0);" class="show-submenu">Home</a>
-                                    <ul>
-                                        <li><a href="index.html">Home Video background</a>
-                                        </li>
-                                        <li><a href="index_2.html">Home Layer Slider</a>
-                                        </li>
-                                        <li><a href="index_3.html">Home Full Header</a>
-                                        </li>
-                                        <li><a href="index_4.html">Home Popup</a>
-                                        </li>
-                                        <li><a href="index_5.html">Home Cookie bar</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="submenu">
-                                    <a href="javascript:void(0);" class="show-submenu">Tours</a>
-                                    <ul>
-                                        <li><a href="grid.html">Grid view</a>
-                                        </li>
-                                        <li><a href="list.html">List view</a>
-                                        </li>
-                                        <li><a href="detail-page.html">Tour Detail</a>
-                                        </li>
-                                    </ul>
-                                </li>
+                                <li><a href="{{ route('tourist.browse-experiences') }}">Home</a></li>
+                                <li><a href="{{ route('tourist.browse-experiences') }}">Tours</a></li>
+                                <li><a href="{{ route('tourist.about') }}">About Us</a></li>
+                                <li><a href="{{ route('tourist.faq') }}">FAQ</a></li>
+                                <li><a href="">Contact Us</a></li>
                                 <li>
-                                    <a href="{{ route('tourist.about') }}">About us</a>
-                                </li>
-                                <li><a href="{{ route('tourist.faq') }}">Faq</a>
-                                </li>
-                                <li class="submenu">
-                                    <a href="javascript:void(0);" class="show-submenu">Other pages</a>
-                                    <ul>
-                                        <li><a href="index_3.html">Header Version 2</a>
-                                        </li>
-                                        <li><a href="blog.html">Blog</a>
-                                        </li>
-                                        <li><a href="blog_post.html">Blog post</a>
-                                        </li>
-                                        <li><a href="gallery.html">Gallery</a>
-                                        </li>
-                                        <li><a href="maintenance.html">Mantainance</a>
-                                        </li>
-                                        <li><a href="profile.html">Team Profile</a>
-                                        </li>
-                                        <li><a href="contacts_2.html">Contact 2</a>
-                                        </li>
-                                        <li><a href="coming_soon/index.html">Coming soon</a>
-                                        </li>
-                                        <li><a href="shortcodes.html">Shortcodes</a>
-                                        </li>
-                                        <li><a href="icon_pack_1.html">Icon pack 1</a>
-                                        </li>
-                                        <li><a href="icon_pack_2.html">Icon pack 2</a>
-                                        </li>
-                                        <li><a href="icon_pack_3.html">Icon pack 3</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="contacts.html">Contact us</a>
-                                </li>
-                                <li class="megamenu submenu">
-                                    <a href="javascript:void(0);" class="show-submenu-mega">More demos</a>
-                                    <div class="menu-wrapper">
-                                        <div class="row">
-                                            <div class="col-md-4">
-                                                <h3>Museum Tours</h3>
-                                                <div class="menu-item">
-                                                    <a href="#"><img scr="assets/tourist/img/menu-demo-1.jpg"
-                                                            width="400" height="226" alt="" class="img-responsive">
-                                                    </a>
-                                                    <p>
-                                                        Lorem ipsum dolor sit amet, et cum civibus referrentur, at
-                                                        propriae forensibus qui. Duo aliquip necessitatibus ne.
-                                                    </p>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-4">
-                                                <h3>Adventure Tours</h3>
-                                                <div class="menu-item">
-                                                    <a href="#"><img scr="assets/tourist/img/menu-demo-2.jpg"
-                                                            width="400" height="226" alt="" class="img-responsive">
-                                                    </a>
-                                                    <p>
-                                                        Lorem ipsum dolor sit amet, et cum civibus referrentur, at
-                                                        propriae forensibus qui. Duo aliquip necessitatibus ne.
-                                                    </p>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-4">
-                                                <h3>Travel Tours</h3>
-                                                <div class="menu-item">
-                                                    <a href="#"><img scr="assets/tourist/img/menu-demo-3.jpg"
-                                                            width="400" height="226" alt="" class="img-responsive">
-                                                    </a>
-                                                    <p>
-                                                        Lorem ipsum dolor sit amet, et cum civibus referrentur, at
-                                                        propriae forensibus qui. Duo aliquip necessitatibus ne.
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <hr class="hidden-xs">
-                                        <p class="text-center hidden-xs">
-                                            <a href="#" class="btn_outline">MORE DEMOS SOON</a>
-                                    </div>
-                                    <!-- End menu-wrapper -->
+                                    <!-- Button to Login Page or Dashboard -->
+                                    <a href="{{ Auth::check() ? route(Auth::user()->role . '.dashboard') : route('login') }}"
+                                        class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                                        {{ Auth::check() ? 'Go to Dashboard' : 'Log In' }}
+                                    </a>
                                 </li>
                             </ul>
                         </div>
@@ -237,58 +129,42 @@
             </div>
             <!-- container -->
         </header>
+
         <!-- End Header -->
     </div>
     <!-- End Header 1-->
 
     {{ $slot }}
 
-    <p>Helo</p>
+
 
     <footer>
         <div class="container">
             <div class="row">
                 <div class="col-md-3 col-sm-3">
-                    <h3>Need help?</h3>
-                    <a href="tel://004542344599" id="phone">+45 423 445 99</a>
-                    <a href="mailto:help@citytours.com" id="email_footer">help@bestours.com</a>
+                    <h3>Need Help?</h3>
+                    <a href="tel://+256123456789" id="phone">+256 123 456 789</a>
+                    <a href="mailto:support@ecotouruganda.com" id="email_footer">support@ecotouruganda.com</a>
                 </div>
                 <div class="col-md-2 col-sm-3">
                     <h3>About</h3>
                     <ul>
-                        <li><a href="#">About us</a>
+                        <li><a href="{{ route('tourist.about') }}">About Us</a></li>
+                        <li><a href="{{ route('tourist.faq') }}">FAQ</a></li>
+                        <li><a
+                                href="{{ Auth::check() ? route(Auth::user()->role . '.dashboard') : route('login') }}">Login</a>
                         </li>
-                        <li><a href="#">FAQ</a>
-                        </li>
-                        <li><a href="#">Login</a>
-                        </li>
-                        <li><a href="">Register</a>
-                        </li>
-                        <li><a href="#">Terms and condition</a>
-                        </li>
+                        <li><a href="{{ route('register') }}">Register</a></li>
                     </ul>
                 </div>
-                <div class="col-md-4 col-sm-6">
-                    <h3>Twitter feed</h3>
-                    <div class="latest-tweets" data-number="10" data-username="ansonika" data-mode="fade"
-                        data-pager="false" data-nextselector=".tweets-next" data-prevselector=".tweets-prev"
-                        data-adaptiveheight="true">
-                        <!-- data-username="your twitter username" -->
-                    </div>
-                    <div class="tweet-control">
-                        <div class="tweets-prev"></div>
-                        <div class="tweets-next"></div>
-                    </div>
-                    <!-- End .tweet-control -->
-                </div>
+
                 <div class="col-md-3 col-sm-12">
                     <h3>Newsletter</h3>
-                    <div id="message-newsletter_2">
-                    </div>
+                    <div id="message-newsletter_2"></div>
                     <form method="post" action="assets/newsletter.php" name="newsletter_2" id="newsletter_2">
                         <div class="form-group">
                             <input name="email_newsletter_2" id="email_newsletter_2" type="email" value=""
-                                placeholder="Your email" class="form-control">
+                                placeholder="Your email" class="form-control" required>
                         </div>
                         <input type="submit" value="Subscribe" class="btn_1" id="submit-newsletter_2">
                     </form>
@@ -301,23 +177,22 @@
                     <div class="styled-select">
                         <select class="form-control" name="lang" id="lang">
                             <option value="English" selected>English</option>
-                            <option value="French">French</option>
-                            <option value="Spanish">Spanish</option>
-                            <option value="Russian">Russian</option>
+                            <option value="Luganda">Luganda</option>
+                            <option value="Swahili">Swahili</option>
                         </select>
                     </div>
-                    <span id="copy">© BestTours 2021 - All rights reserved</span>
+                    <span id="copy">© EcoTour Uganda <span id="currentYear"></span> - All rights reserved</span>
                 </div>
                 <div class="col-sm-4" id="social_footer">
                     <ul>
-                        <li><a href="#"><i class="icon-facebook"></i></a>
+                        <li><a href="https://www.facebook.com/EcoTourUganda" target="_blank"><i
+                                    class="icon-facebook"></i></a></li>
+                        <li><a href="https://twitter.com/EcoTourUganda" target="_blank"><i class="icon-twitter"></i></a>
                         </li>
-                        <li><a href="#"><i class="icon-twitter"></i></a>
-                        </li>
-                        <li><a href="#"><i class="icon-google"></i></a>
-                        </li>
-                        <li><a href="#"><i class="icon-instagram"></i></a>
-                        </li>
+                        <li><a href="https://plus.google.com/EcoTourUganda" target="_blank"><i
+                                    class="icon-google"></i></a></li>
+                        <li><a href="https://www.instagram.com/EcoTourUganda" target="_blank"><i
+                                    class="icon-instagram"></i></a></li>
                     </ul>
                 </div>
             </div>
@@ -325,6 +200,7 @@
         </div>
         <!-- End container -->
     </footer>
+
     <!-- End footer -->
 
     <div id="toTop"></div>
@@ -360,9 +236,12 @@
         });
     </script>
 
+    <script>
+        document.getElementById('currentYear').textContent = new Date().getFullYear();
+    </script>
+
     @livewireScripts
     @stack('scripts')
-
 </body>
 
 </html>
--- resources/views/livewire/tourist/browse-experiences.blade.php
+++ resources/views/livewire/tourist/browse-experiences.blade.php
@@ -1,3 +1,19 @@
 <div>
-    {{-- Do your work, then step back. --}}
+    <!-- SubHeader =============================================== -->
+    <section class="header-video">
+        <div id="hero_video">
+            <div id="animate_intro">
+                <h3>Enjoy a Perfect Tour</h3>
+                <p>
+                    Find the best Tours and Excursion at the best price
+                </p>
+            </div>
+        </div>
+        <img src="{{ asset('assets/tourist/img/video_fix.png')}}" alt="" class="header-video--media"
+            data-video-src="{{ asset('assets/tourist/video/intro')}}"
+            data-teaser-source="{{ asset('assets/tourist/video/intro')}}" data-provider="" data-video-width="1920"
+            data-video-height="750">
+    </section>
+    <!-- End Header video -->
+    <!-- End SubHeader ============================================ -->
 </div>
